f909e13c804d123bb6b09ab08438970f639ec1b4
James Wang <jmaeswnag@gmail.com>
2024-05-19 18:44:40 -0500

    Adding functionality for users to "sketch" by mousing over cells

--- index.js
+++ index.js
@@ -1,21 +1,43 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const GRID_WIDTH = 16;
-    const GRID_HEIGHT = 16;
+    let sideWidth = 16;
     const body = document.querySelector("body");
 
     function createGrid() {
         const divGridContainer = document.createElement("div");
         divGridContainer.classList.add("gridContainer");
         body.appendChild(divGridContainer);
-        for(let i = 0; i < (GRID_WIDTH * GRID_HEIGHT) ; i++) {
+        for(let i = 0; i < (sideWidth * sideWidth) ; i++) {
             let currCell = document.createElement("div");
             currCell.classList.add("gridCell");
+            currCell.addEventListener("mouseover", colorCell);
+            currCell.style.flexGrow = "1";
+            currCell.style.flexShrink = "1";
+            currCell.style.flexBasis = `${1000/(sideWidth + 1)}px`;
+            //flex: 1 1 calc((1000px)/ 17);
+
             divGridContainer.appendChild(currCell);
         }
     }
 
+    function colorCell(e) {
+        e.target.classList.add("coloredGridCell");
+    }
+
+    function resetGrid(e) {
+        inputSideWidthNumber = document.querySelector("#sideWidth");
+        if(parseFloat(inputSideWidthNumber.value) < 1 ||
+           parseFloat(inputSideWidthNumber.value) > 100) {
+            //Illegal values!
+            inputSideWidthNumber.value = sideWidth;
+            alert("Please enter a valid side width (1-100)!")
+
+        } 
+    }
+
     function main() {
         createGrid();
+        const submitButton = document.querySelector("#submitSideWidth");
+        submitButton.addEventListener("click", resetGrid);
     }
 
     main();
